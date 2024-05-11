be4478c42a20e79855c083fe74722d7f8cfdf69b
808_dita <808_dita@DESKTOP-GNE71AK>
2024-05-11 17:36:05 +0900

    gd

--- sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -2,6 +2,7 @@ package com.mysite.sbb.answer;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/answer")
@@ -17,14 +19,19 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class AnswerController {
 
-	private final QuestionService questionService;
-	private final AnswerService answerService;
-	
-	@PostMapping("/create/{id}")
-	public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
-		Question question = this.questionService.getQuestion(id);
-		// TODO: 답변 삭제 Service 메소드
-		this.answerService.create(question,  content);
-		return String.format("redirect:/question/detail/%s",id);
-	}
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+    
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+
+        if (bindingResult.hasErrors()) {
+            return "question_detail"; // 변경: 에러가 있을 때 해당 페이지로 포워딩
+        }
+        
+        this.answerService.create(question, answerForm.getContent());
+        return String.format("redirect:/question/detail/%s", id);
+    }
 }
--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -14,6 +14,7 @@ import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.answer.AnswerRepository;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 
 import jakarta.transaction.Transactional;
 
@@ -23,23 +24,31 @@ import java.util.Optional;
 class SbbApplicationTests {
 	
 	@Autowired
-	private QuestionRepository questionRepository;
+	private QuestionService questionService;
 	
-	@Autowired
-	private AnswerRepository answerRepository;
+//	@Autowired
+//	private QuestionRepository questionRepository;
+//	
+//	@Autowired
+//	private AnswerRepository answerRepository;
 
 	@Transactional
 	@Test
 	void testJpa() {
+		for(int i=1; i<=300; i++) {
+			String subject = String.format("테스트 데이터입니다 : [%03d]", i);
+			String content = "내용 없음";
+			this.questionService.create(subject, content);
+		}
 		
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-		
-		List<Answer> answerList = q.getAnswerList();
-		
-		assertEquals(1,answerList.size());
-		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+//		Optional<Question> oq = this.questionRepository.findById(2);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//		
+//		List<Answer> answerList = q.getAnswerList();
+//		
+//		assertEquals(1,answerList.size());
+//		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 		
 //		Optional<Answer> oa = this.answerRepository.findById(1);
 //		assertTrue(oa.isPresent());
